60e68890667998ec3692729308537633a45b463c
Nghia Tran <nghia@proxyman.com>
2026-04-23 12:43:23 +0200

    Initial Commit

---
+++ Packetry/PacketryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PacketryApp.swift
+//  Packetry
+//
+//  Created by nghiatran on 23/4/26.
+//
+
+import SwiftUI
+
+@main
+struct PacketryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PacketryTests/PacketryTests.swift
@@ -0,0 +1,19 @@
+//
+//  PacketryTests.swift
+//  PacketryTests
+//
+//  Created by nghiatran on 23/4/26.
+//
+
+import Testing
+@testable import Packetry
+
+struct PacketryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
