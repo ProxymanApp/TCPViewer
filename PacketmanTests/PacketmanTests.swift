f089b3c53cded9f343d7dfd9340155ad0dba2746
Nghia Tran <nghia@proxyman.com>
2026-04-24 12:48:53 +0200

    Add macOS Target app

---
+++ PacketmanTests/PacketmanTests.swift
@@ -0,0 +1,19 @@
+//
+//  PacketmanTests.swift
+//  PacketmanTests
+//
+//  Created by nghiatran on 24/4/26.
+//
+
+import Testing
+@testable import Packetman
+
+struct PacketmanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
